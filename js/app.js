fd90ab2dbc2558dda88767ebef1b4b6c388227e3
praveenkumar <praveenkumar7501@gmail.com>
2020-07-19 14:52:18 +0530

    19-july-20

--- js/app.js
+++ js/app.js
@@ -1,4 +1,128 @@
+
+particlesJS('particles-js',
+
+{
+  "particles": {
+    "number": {
+      "value": 47,
+      "density": {
+        "enable": true,
+        "value_area": 1026
+      }
+    },
+    "color": {
+      "value": "#61babc"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true,
+  "config_demo": {
+    "hide_card": false,
+    "background_color": "#08182f",
+    "background_image": "",
+    "background_position": "50% 50%",
+    "background_repeat": "no-repeat",
+    "background_size": "cover"
+  }
+}
+
+);
+
 $(document).ready(function() {
+
+    $('#home-work span').hide();
 
     window.onscroll = function() {scrollFunction()};
 	function scrollFunction() {
@@ -65,8 +189,16 @@ $(document).ready(function() {
         } else {
             $('#process-img2').removeClass('animate-slideToRight');
         }
-    }
 
+        if (document.body.scrollTop > 2600 || document.documentElement.scrollTop > 2600) {
+            $('#home-work span').each(function(index) {
+                var newlist = $(this);
+                setTimeout(function() {
+                    $(newlist).fadeIn();
+                }, index*700);
+            }); 
+        }
+    }
 
     
     // work process infinite timeline
@@ -92,6 +224,9 @@ $(document).ready(function() {
     timelineDotLooping();
 
 });
+
+/* Author: Praveenkumar C */
+
 
 
 
