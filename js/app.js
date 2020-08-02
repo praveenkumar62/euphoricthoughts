b2431fcdc4b91592dc6260f4f01ff25dd327bb3f
praveenkumar <praveenkumar7501@gmail.com>
2020-08-02 20:24:09 +0530

    2-8-20

--- js/app.js
+++ js/app.js
@@ -101,129 +101,51 @@ $(document).ready(function() {
   }
   timelineDotLooping();
 
+//   Product Engg Services 
+  function servicesInteractiveOne() {
+    var $servicesList = $('#services-interactive1 .services-circle')
+    $servicesList.each(function(index) {
+        var items = $(this);
+        setTimeout(function() {
+            $('#services-interactive1 .animate-scaleUp').removeClass('animate-scaleUp');
+            $(items).addClass('animate-scaleUp');
+            
+            if(index == $servicesList.length-1) {
+                setTimeout(function() {
+                    servicesInteractiveOne();
+                }, 4000);
+            }
+        },index*4000);
+        
+    });
+  }
+  function servicesInteractiveTwo() {
+    var $servicesList2 = $('#services-interactive2 .services-circle');
+    $servicesList2.each(function(index) {
+        var items = $(this);
+        setTimeout(function() {
+            $('#services-interactive2 .animate-scaleUp').removeClass('animate-scaleUp');
+            $(items).addClass('animate-scaleUp');
 
+            if(index == $servicesList2.length-1) {
+                setTimeout(function() {
+                    servicesInteractiveTwo();
+                }, 4000);
+            }
+        },index*4000)
+    })
+  }
+  
+  servicesInteractiveOne();
+  setTimeout(function() {
+    servicesInteractiveTwo();
+  },2000);
+
+
+//   Enabling tooltip
+$('[data-toggle="tooltip"]').tooltip();
 
 });
 
+
 /* Author: Praveenkumar C */
-
-
-
-
-// $(document).ready(function() {
-//     $('#home-card1').mouseover(function() {
-//         $('#sub-leftcard1').removeClass('hide-sub-homecard');
-//         $('#sub-leftcard1').addClass('sub-leftcard1');
-//     });
-//     $('#home-card1').mouseout(function() {
-//         $('#sub-leftcard1').removeClass('sub-leftcard1');
-//         $('#sub-leftcard1').addClass('hide-sub-homecard');
-//     })
-//     $('#home-card1').mouseover(function() {
-//         $('#sub-leftcard2').removeClass('hide-sub-homecard');
-//         $('#sub-leftcard2').addClass('sub-leftcard2');
-//     });
-//     $('#home-card1').mouseout(function() {
-//         $('#sub-leftcard2').removeClass('sub-leftcard2');
-//         $('#sub-leftcard2').addClass('hide-sub-homecard');
-//     })
-//     $('#home-card1').mouseover(function() {
-//         $('#sub-leftcard3').removeClass('hide-sub-homecard');
-//         $('#sub-leftcard3').addClass('sub-leftcard3');
-//     });
-//     $('#home-card1').mouseout(function() {
-//         $('#sub-leftcard3').removeClass('sub-leftcard3');
-//         $('#sub-leftcard3').addClass('hide-sub-homecard');
-//     })
-
-//     $('#home-card2').mouseover(function() {
-//         $('#sub-centercard1').removeClass('hide-sub-homecard');
-//         $('#sub-centercard1').addClass('sub-centercard1');
-//     });
-//     $('#home-card2').mouseout(function() {
-//         $('#sub-centercard1').removeClass('sub-centercard1');
-//         $('#sub-centercard1').addClass('hide-sub-homecard');
-//     })
-//     $('#home-card2').mouseover(function() {
-//         $('#sub-centercard2').removeClass('hide-sub-homecard');
-//         $('#sub-centercard2').addClass('sub-centercard2');
-//     });
-//     $('#home-card2').mouseout(function() {
-//         $('#sub-centercard2').removeClass('sub-centercard2');
-//         $('#sub-centercard2').addClass('hide-sub-homecard');
-//     })
-//     $('#home-card2').mouseover(function() {
-//         $('#sub-centercard3').removeClass('hide-sub-homecard');
-//         $('#sub-centercard3').addClass('sub-centercard3');
-//     });
-//     $('#home-card2').mouseout(function() {
-//         $('#sub-centercard3').removeClass('sub-centercard3');
-//         $('#sub-centercard3').addClass('hide-sub-homecard');
-//     })
-//     $('#home-card2').mouseover(function() {
-//         $('#sub-centercard4').removeClass('hide-sub-homecard');
-//         $('#sub-centercard4').addClass('sub-centercard4');
-//     });
-//     $('#home-card2').mouseout(function() {
-//         $('#sub-centercard4').removeClass('sub-centercard4');
-//         $('#sub-centercard4').addClass('hide-sub-homecard');
-//     })
-
-//     $('#home-card3').mouseover(function() {
-//         $('#sub-rightcard1').removeClass('hide-sub-homecard');
-//         $('#sub-rightcard1').addClass('sub-rightcard1');
-//     });
-//     $('#home-card3').mouseout(function() {
-//         $('#sub-rightcard1').removeClass('sub-rightcard1');
-//         $('#sub-rightcard1').addClass('hide-sub-homecard');
-//     })
-//     $('#home-card3').mouseover(function() {
-//         $('#sub-rightcard2').removeClass('hide-sub-homecard');
-//         $('#sub-rightcard2').addClass('sub-rightcard2');
-//     });
-//     $('#home-card3').mouseout(function() {
-//         $('#sub-rightcard2').removeClass('sub-rightcard2');
-//         $('#sub-rightcard2').addClass('hide-sub-homecard');
-//     })
-//     $('#home-card3').mouseover(function() {
-//         $('#sub-rightcard3').removeClass('hide-sub-homecard');
-//         $('#sub-rightcard3').addClass('sub-rightcard3');
-//     });
-//     $('#home-card3').mouseout(function() {
-//         $('#sub-rightcard3').removeClass('sub-rightcard3');
-//         $('#sub-rightcard3').addClass('hide-sub-homecard');
-//     })
-//     $('#home-card3').mouseover(function() {
-//         $('#sub-rightcard4').removeClass('hide-sub-homecard');
-//         $('#sub-rightcard4').addClass('sub-rightcard4');
-//     });
-//     $('#home-card3').mouseout(function() {
-//         $('#sub-rightcard4').removeClass('sub-rightcard4');
-//         $('#sub-rightcard4').addClass('hide-sub-homecard');
-//     });
-
-
-
-//     function timelineDotLooping() {
-//         var $datalist = $('#home-work-dot .work-dot');
-//         $.each($datalist, function(index){
-//             var self = this;
-//             setTimeout(function () {
-
-//                 $(self).addClass('purple-work-dot');
-//                 $(self).parent().prev().find('.work-dot').removeClass('purple-work-dot');
-//                 if(index == 0) {
-//                     $datalist.last().removeClass('purple-work-dot');
-//                 }
-//                 // for infinite times
-//                 if(index == $datalist.length-1) {
-//                     setTimeout(function () {
-//                         timelineDotLooping();
-//                     }, 3000);
-//                 }
-//             }, index*3000);
-//         });
-//     }
-//     timelineDotLooping();
-
-// });
